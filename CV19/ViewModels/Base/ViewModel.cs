5da6dd9073ac5e82b6fb846124f27f8026f2069a
ems <ems82@ya.ru>
2020-12-16 22:14:35 +0300

    Add IDisposable in class ViewModel

--- CV19/ViewModels/Base/ViewModel.cs
+++ CV19/ViewModels/Base/ViewModel.cs
@@ -6,21 +6,33 @@ using System.Runtime.CompilerServices;
 
 namespace CV19.ViewModels.Base
 {
-    internal abstract class ViewModel : INotifyPropertyChanged
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void OnPropertyChanged( [CallerMemberName] string PropertyName = null)
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
         }
-    protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
         {
             if (Equals(field, value)) return false;
             field = value;
             OnPropertyChanged(PropertyName);
             return true;
         }
-
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        private bool _Disposed;
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+        }
+    }
 
 }
