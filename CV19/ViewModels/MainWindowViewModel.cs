b9a3dd487eaf4d501ee3d905061e8b764b0ffc07
ems <ems82@ya.ru>
2020-12-17 22:44:07 +0300

    Binding Title

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using CV19.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CV19.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
+        private string _Title = "Анализ статистики" ;
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //    //if (Equals(_Title, value)) return; // но у нас есть метод Set
+            //    //_Title = value;
+            //    //OnPropertyChanged();
+
+            //    //Set(ref _Title, value); // и можно написать так 
+            //}
+            set => Set(ref _Title, value);
+        }
+        #endregion
+
+
     }
 }
