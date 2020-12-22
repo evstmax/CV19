5d3f667ef4b055e67ce8b66322ad5a5f9952cd85
ems <ems82@ya.ru>
2020-12-22 22:26:20 +0300

    Криязка комманды закрыть к  Элипсу

---
+++ CV19/Ifrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using CV19.Ifrastructure.Commands.Base;
+
+namespace CV19.Ifrastructure.Commands
+{
+   internal class CloseApplicationCommand : Command
+   {
+       public override bool CanExecute(object parameter) => true;
+
+
+
+       public override void Execute(object parameter) => Application.Current.Shutdown();
+
+   }
+}
