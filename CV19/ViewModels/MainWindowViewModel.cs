30b49c5ec31e75f17b1e5a2968aa894a271c9f5f
ems <ems82@ya.ru>
2021-01-09 01:08:21 +0300

    Добавлен пример визуализации Oxi Plot

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,29 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Input;
 using CV19.Ifrastructure.Commands;
+using CV19.Models;
 using CV19.ViewModels.Base;
 
 namespace CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region TestDataPoints : IEnumerable<DataPoint> - Тестовый набор данных для визуализации графиков
+
+        /// <summary>Тестовый набор данных для визуализации графиков</summary>
+        private IEnumerable<DataPoint> _TestDataPoints;
+
+        /// <summary>Тестовый набор данных для визуализации графиков</summary>
+        public IEnumerable<DataPoint> TestDataPoints
+        {
+            get => _TestDataPoints;
+            set => Set(ref _TestDataPoints, value);
+        }
+
+        #endregion
+
         #region Заголовок окна
         private string _Title = "Анализ статистики CV19" ;
         /// <summary>
@@ -66,9 +83,19 @@ namespace CV19.ViewModels
 
         CloseApplicationCommand  = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
 
-
         #endregion
-    }
+
+
+            var data_points = new List<DataPoint>((int)(360 / 0.1));
+        for (var x = 0d; x <= 360; x += 0.1)
+        {
+            const double to_rad = Math.PI / 180;
+            var y = Math.Cos(x * to_rad);
+           data_points.Add(new DataPoint {XValue = x, YValue = y}); 
+        }
+
+        TestDataPoints = data_points;
+        }
 }
 
 
