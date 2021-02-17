a6bdbf4114807b3ff59ba0639f730139cefd56cd
ems <ems82@ya.ru>
2021-02-17 22:06:14 +0300

    Реализация фильтраций групп студентов в разметке и методов обработчиков событий Filter и Changed

--- CV19/Views/Windows/MainWindow.xaml.cs
+++ CV19/Views/Windows/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CV19.Models.Decanat;
 
 namespace CV19
 {
@@ -28,6 +29,28 @@ namespace CV19
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+
+
+        private void GroupsCollectionFilter(object sender, FilterEventArgs e)
+        {
+            if (!(e.Item is Group group)) return;
+            if (group.Name is null) return;
+
+            var filter_text = GroupNameFilterText.Text;
+            if (filter_text.Length == 0) return;
+
+            if (group.Name.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+            if (group.Description !=null && group.Description.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+
+            e.Accepted = false;
+        }
+
+        private void OnGroupsFilterTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var text_box = (TextBox) sender;
+            var collection = (CollectionViewSource)text_box.FindResource("GroupsCollection");
+            collection.View.Refresh();
         }
     }
 }
